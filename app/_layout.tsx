bd263f3584202252569ca55d488c36da27ed77bb
xuerongNanopay <xuerong@nanopay.net>
2024-07-29 20:38:49 -0400

    bug fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,22 +9,16 @@
 // }
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Slot } from 'expo-router'
 
 const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>Xuerong</Text>
-    </View>
+    <>
+      <Text>Header</Text>
+      <Slot/>
+      <Text>Footer</Text>
+    </>
   )
 }
 
 export default RootLayout
-
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Xuerong</Text>
+    <View style={styles.container}>
+      <Text>Xuerong from index</Text>
+      <StatusBar style="auto" />
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
