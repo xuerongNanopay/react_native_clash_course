986fb2673f3794a5671b797319e7efe0957030b6
xuerongNanopay <xuerong@nanopay.net>
2024-07-30 19:52:18 -0400

    save change

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,11 @@
-// import { Stack } from "expo-router";
-
-// export default function RootLayout() {
-//   return (
-//     <Stack>
-//       <Stack.Screen name="index" />
-//     </Stack>
-//   );
-// }
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Slot } from 'expo-router'
+import { Stack } from 'expo-router'
 
 const RootLayout = () => {
   return (
-    <>
-      <Text>Header</Text>
-      <Slot/>
-      <Text>Footer</Text>
-    </>
+    <Stack>
+       <Stack.Screen name="index" options={{headerShown: false}}/>
+     </Stack>
   )
 }
 
