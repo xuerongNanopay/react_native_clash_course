f151a8f612f537a4e0ab5b441ac8b9d231f7a387
xuerongNanopay <xuerong@nanopay.net>
2024-07-30 20:02:17 -0400

    add profile

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
+import { Link } from 'expo-router'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Xuerong from index</Text>
       <StatusBar style="auto" />
+      <Link href="/profile">Go to Profile</Link>
     </View>
   )
 }
